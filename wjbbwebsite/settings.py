4c533958dd26ace58fb947f059bb9051947a22f6
test <test@172-18-8-153.(none)>
2014-03-03 12:58:49 +0800

    use django-registration to send activate email

--- wjbbwebsite/settings.py
+++ wjbbwebsite/settings.py
@@ -46,9 +46,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 
 # Optional SMTP authentication information for EMAIL_HOST.
-#EMAIL_HOST_USER = '278006819@qq.com'
-EMAIL_HOST_USER = 'xueyu7452@gmail.com'
-EMAIL_HOST_PASSWORD = 'PPT200741'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 LOGIN_REDIRECT_URL = '/sellers/'
